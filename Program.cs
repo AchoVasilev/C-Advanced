0b2064baeb3f615a4fa01210fc8d998c92c5ed90
AngelVasilev <58532362+AchoVasilev@users.noreply.github.com>
2020-05-21 16:17:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_5._Applied_Arithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            Func<List<int>, List<int>> add = x => x.Select(y => y + 1).ToList();
+            Func<List<int>, List<int>> multiply = x => x.Select(y => y * 2).ToList();
+            Func<List<int>, List<int>> subtract = x => x.Select(y => y - 1).ToList();
+            Func<List<int>, string> print = x => String.Join(" ", x);
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "end")
+                {
+                    break;
+                }
+
+                switch (command)
+                {
+                    case "add":
+                        input = add(input);
+                        break;
+
+                    case "multiply":
+                        input = multiply(input);
+                        break;
+
+                    case "subtract":
+                        input = subtract(input);
+                        break;
+
+                    case "print":
+                        Console.WriteLine(print(input));
+                        break;
+                }
+            }
+        }
+    }
+}
